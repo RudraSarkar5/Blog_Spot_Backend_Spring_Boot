b6459aa24b98e9e78d50a6ee5b45770732c82d2c
RudraSarkar5 <rudrasarkar311@gmail.com>
2024-08-14 08:58:18 +0530

    setting up spring project with application properties

---
+++ src/main/java/com/example/Blog_Spot_Backend/BlogSpotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Blog_Spot_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSpotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogSpotBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Blog_Spot_Backend/BlogSpotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Blog_Spot_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSpotBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
